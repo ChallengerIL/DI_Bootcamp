354738b4787d4d65e12db40e3bfc1bcff03d1fd6
ChallengerIL <zacsergo@gmail.com>
2023-04-30 10:16:12 +0300

    Finished the first mandatory exercise from W2D1

---
+++ Week2/Day1/MandatoryExercise/exercise5.py
@@ -0,0 +1,6 @@
+name = "Iurii"
+age = 30
+shoe_size = 11.5
+info = f"My name is {name}, I am {age} years old and my shoe size is {shoe_size}"
+
+print(info)
---
+++ Week2/Day1/MandatoryExercise/exercise8.py
@@ -0,0 +1,7 @@
+base_name = "Iurii"
+user_name = input("Please enter your name: ").title()
+
+if base_name == user_name:
+    print("Awesome! We have the same name!")
+else:
+    print("Our names are different... What a shame.")
