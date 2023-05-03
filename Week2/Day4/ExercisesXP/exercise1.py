0e0b206ff7a2d71239ba00a1f5de5ecaa407394f
ChallengerIL <zacsergo@gmail.com>
2023-05-03 09:56:39 +0300

    Finished Exercise1 from XPExercises from W2D4

---
+++ Week2/Day4/ExercisesXP/exercise1.py
@@ -0,0 +1,10 @@
+# 1. Write a function called display_message() that prints one sentence telling everyone what you are learning in this course.
+# 2. Call the function, and make sure the message displays correctly.
+
+
+def display_message():
+    print("I am learning Python")
+
+
+if __name__ == "__main__":
+    display_message()
