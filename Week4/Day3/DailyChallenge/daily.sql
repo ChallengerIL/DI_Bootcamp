60577b8024ba0a20cc28f55500e29fc2e67ab8ba
ChallengerIL <zacsergo@gmail.com>
2023-05-15 12:23:00 +0300

    Finished the Daily Challenge from W4D3

--- Week4/Day3/DailyChallenge/daily.sql
+++ Week4/Day3/DailyChallenge/daily.sql
@@ -1,3 +1,4 @@
+-- Tables Relationships
 -- You are going to practice tables relationships
 
 -- Part I
@@ -125,7 +126,7 @@ CREATE TABLE book (
 -- 2.2
 INSERT INTO book (title, author)
 VALUES
-	('Alice in Wonderland', 'Lewis Carroll'),
+	('Alice In Wonderland', 'Lewis Carroll'),
 	('Harry Potter', 'J.K Rowling'),
 	('To kill a mockingbird', 'Harper Lee');
 
@@ -155,4 +156,92 @@ CREATE TABLE library (
 );
 
 -- 2.6
+INSERT INTO library (book_fk_id, student_fk_id, borrowed_date)
+VALUES 
+	((SELECT book_id FROM book where title = 'Alice In Wonderland'), 
+	(SELECT student_id FROM student where name = 'John'), '2022-02-15'),
+	((SELECT book_id FROM book where title = 'To kill a mockingbird'), 
+	(SELECT student_id FROM student where name = 'Bob'), '2021-03-03'),
+	((SELECT book_id FROM book where title = 'Alice In Wonderland'), 
+	(SELECT student_id FROM student where name = 'Lera'), '2021-05-23'),
+	((SELECT book_id FROM book where title = 'Harry Potter'), 
+	(SELECT student_id FROM student where name = 'Bob'), '2021-08-12');
 
+-- 2.7
+SELECT * FROM library;
+
+SELECT s.name, b.title
+FROM student s
+INNER JOIN library l
+ON s.student_id = l.student_fk_id
+INNER JOIN book b
+ON b.book_id = l.book_fk_id;
+
+SELECT AVG(s.age)
+FROM student s
+INNER JOIN library l
+ON s.student_id = l.student_fk_id
+INNER JOIN book b
+ON b.book_id = l.book_fk_id
+WHERE b.title = 'Alice In Wonderland';
+
+DELETE FROM student
+WHERE name = 'John';
+-- The student's record got deleted
+
+
+-- Items & Orders
+-- Create a table called product_orders and a table called items. You decide which fields should be in each table, although make sure the items table has a column called price.
+
+-- There should be a one to many relationship between the product_orders table and the items table. An order can have many items, but an item can belong to only one order.
+
+-- Create a function that returns the total price for a given order.
+
+-- Bonus :
+-- Create a table called users.
+-- There should be a one to many relationship between the users table and the product_orders table.
+-- Create a function that returns the total price for a given order of a given user.
+
+-- 1
+CREATE TABLE product_orders (
+	order_id SERIAL PRIMARY KEY,
+	created_on DATE NOT NULL
+);
+
+-- 2
+CREATE TABLE items (
+	item_id serial PRIMARY KEY,
+    fk_order_id INTEGER NOT NULL,
+	price INTEGER NOT NULL,
+    FOREIGN KEY (fk_order_id) REFERENCES product_orders(order_id)
+);
+
+INSERT INTO product_orders (created_on)
+VALUES 
+	('2022-02-20'),
+	('2023-07-12'),
+	('2021-11-11');
+
+INSERT INTO items (fk_order_id, price)
+SELECT product_orders.order_id, 99
+FROM product_orders
+WHERE product_orders.order_id = 1;
+
+INSERT INTO items (fk_order_id, price)
+SELECT product_orders.order_id, 50
+FROM product_orders
+WHERE product_orders.order_id = 1;
+
+INSERT INTO items (fk_order_id, price)
+SELECT product_orders.order_id, 27
+FROM product_orders
+WHERE product_orders.order_id = 3;
+
+
+
+-- 3
+CREATE or REPLACE FUNCTION get_full_amount(product_orders_id INTEGER) RETURNS INTEGER AS $fullamount$
+BEGIN
+   RETURN(SELECT SUM(items.price)) FROM items INNER JOIN product_orders ON items.fk_order_id = product_orders.order_id WHERE items.fk_order_id = product_orders_id;
+END;
+$fullamount$ LANGUAGE plpgsql;
