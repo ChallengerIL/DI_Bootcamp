ed1fb1ec591724c548e1ed357a1df8a6dedd9816
ChallengerIL <zacsergo@gmail.com>
2023-05-18 08:10:14 +0300

    Made minor adjustments to the code from W3D5

--- Week3/Day5/DailyChallenge/daily1/oop.py
+++ Week3/Day5/DailyChallenge/daily1/oop.py
@@ -54,28 +54,4 @@ class Deck:
 
 if __name__ == '__main__':
     deck = Deck()
-
-
-# MODULES
-# Using the requests and time modules, create a function which returns the amount of time it takes a webpage to load (how long it takes for a complete response to a request).
-# Test your code with multiple sites such as google, ynet, imdb, etc.
-
-import requests
-import time
-
-URL = 'https://imdb.com'
-
-
-def loading_speed_test(url: str = URL):
-    return requests.get(url).elapsed.total_seconds()
-
-
-def loading_speed_test2(url: str = URL):
-    start = time.time()
-    requests.get(url)
-    end = time.time()
-    return end - start
-
-
-print(loading_speed_test())
-print(loading_speed_test2())
+    
