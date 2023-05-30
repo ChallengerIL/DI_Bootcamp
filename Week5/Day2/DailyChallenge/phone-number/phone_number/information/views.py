d24ce7c6202524c333a0e43f332e58498d6adce7
ChallengerIL <zacsergo@gmail.com>
2023-05-30 08:44:07 +0300

    Added exception handling

--- Week5/Day2/DailyChallenge/phone-number/phone_number/information/views.py
+++ Week5/Day2/DailyChallenge/phone-number/phone_number/information/views.py
@@ -4,16 +4,26 @@ from .models import Person
 
 # http://127.0.0.1:8000/persons/name/Haim%20Uriel/
 def name(request, persons_name):
+    try:
+        person = Person.objects.get(name=persons_name)
+    except Person.DoesNotExist:
+        person = None
+
     context = {
-        'person': Person.objects.get(name=persons_name),
+        'person': person,
     }
     return render(request, 'information/name.html', context)
 
 
 # http://127.0.0.1:8000/persons/phonenumber/+97237510290/
 def phone_number(request, phone):
+    try:
+        person = Person.objects.get(phone_number=phone)
+    except Person.DoesNotExist:
+        person = None
+
     context = {
-        'person': Person.objects.get(phone_number=phone),
+        'person': person,
     }
     return render(request, 'information/phone_number.html', context)
 
