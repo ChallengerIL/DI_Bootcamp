330c566cf9794461f4a91728eb7a23fb03b19dc1
ChallengerIL <zacsergo@gmail.com>
2023-06-12 09:43:07 +0300

    Finished the XP Exercise from W7D1

---
+++ Week7/Day1/ExercisesXP/studentsApp/students_app/students/views.py
@@ -0,0 +1,43 @@
+from .models import Student
+from django.views.decorators.csrf import csrf_exempt
+from .serializers import StudentSerializer
+from django.http import HttpResponse, JsonResponse
+from rest_framework.parsers import JSONParser
+
+
+@csrf_exempt
+def student_list(request):
+    if request.method == "GET":
+        queryset = Student.objects.all()
+        serializer = StudentSerializer(queryset, many=True)
+        return JsonResponse(data=serializer.data, safe=False)
+
+    if request.method == "POST":
+        student_data = JSONParser().parse(request)
+        serializer = StudentSerializer(data=student_data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)  # successfully created
+        return JsonResponse(serializer.errors, status=400)
+
+
+@csrf_exempt
+def student_detail(request, pk):
+    try:
+        student = Student.objects.get(pk=pk)
+    except Student.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = StudentSerializer(student)
+        return JsonResponse(data=serializer.data)
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = StudentSerializer(instance=student, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+    elif request.method == 'DELETE':
+        student.delete()
+        return HttpResponse(status=204)
