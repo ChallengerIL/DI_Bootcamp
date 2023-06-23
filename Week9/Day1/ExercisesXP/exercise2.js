f1874f2261f817e4f18eddc095832057da51c9da
ChallengerIL <zacsergo@gmail.com>
2023-06-23 08:56:01 +0300

    Finished the XP Exercises from W9D1

---
+++ Week9/Day1/ExercisesXP/exercise2.js
@@ -0,0 +1,8 @@
+// Exercise 2 : HTML Form #2
+// Instructions
+// Use the same form structure as Exercise 1.
+// When you click the Send button, the form will be submitted with a POST method.(you can use the same HTML file for the action url)
+// Where will the sent data appear? Hint : Look at the Network Tab
+
+// The answer:
+// This time the data is being included in the request body. You can see it in the Payload tab under "Form Data". 
