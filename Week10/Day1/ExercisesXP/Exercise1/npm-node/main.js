591c801faf760ba182636845ee236a5cfdc1e139
ChallengerIL <zacsergo@gmail.com>
2023-06-25 11:53:58 +0300

    Finished the XP Exercises from W10D1

---
+++ Week10/Day1/ExercisesXP/Exercise1/npm-node/main.js
@@ -0,0 +1,19 @@
+// Exercise 1 : Hello Node.Js
+// Create a new folder called - npm-node
+// Create a JS file called - main.js
+// Add the following code to the file main.js :
+
+// const a = 5;
+// const b = 10;
+
+
+// Write some more code in the main.js file so when you run it in the command prompt it logs Hello you are 15
+
+const a = 5;
+const b = 10;
+
+function tellAge() {
+    console.log(`Hello you are ${a+b}`);
+}
+
+tellAge()
